e8c9a9e29629991a30ae5040f58fbae5f81eaf89
William <119466101+WBexte@users.noreply.github.com>
2023-11-03 15:14:10 -0600

    Completed stack interface

--- src/dataStructures/StackADT.java
+++ src/dataStructures/StackADT.java
@@ -6,6 +6,26 @@ package dataStructures;
 /**
  * 
  */
-public interface StackADT {
-
+public interface StackADT<E> {
+	
+	public void push(E item);
+	
+	public E pop();
+	
+	public E peek();
+	
+	public boolean equals( StackADT<E> that );
+	
+	public Iterator<E> iterator();
+	
+	public Object[] toArray();
+	
+	public int search(E obj);
+	
+	public int size();
+	
+	public boolean isEmpty();
+	
+	public void clear();
+	
 }
