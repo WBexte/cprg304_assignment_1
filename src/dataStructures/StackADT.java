a1f81ef32e5381c438315c2cdb9015180ebf0799
William <119466101+WBexte@users.noreply.github.com>
2023-11-03 15:37:23 -0600

    Added javadoc

--- src/dataStructures/StackADT.java
+++ src/dataStructures/StackADT.java
@@ -4,28 +4,45 @@
 package dataStructures;
 
 /**
- * 
+ * A generic interface representing a Stack data structure.
+ * @param <E> the type of elements stored in the Stack.
  */
 public interface StackADT<E> {
-	
-	public void push(E item);
-	
-	public E pop();
-	
-	public E peek();
-	
-	public boolean equals( StackADT<E> that );
-	
-	public Iterator<E> iterator();
-	
-	public Object[] toArray();
-	
-	public int search(E obj);
-	
-	public int size();
-	
-	public boolean isEmpty();
-	
-	public void clear();
-	
+    /**
+     * Pushes an element onto the top of the Stack.
+     *
+     * @param element the element to be pushed onto the Stack.
+     * @throws NullPointerException if the specified element is null.
+     */
+    void push(E element);
+
+    /**
+     * Removes and returns the element at the top of the Stack.
+     *
+     * @return the element at the top of the Stack.
+     * @throws EmptyStackException if the Stack is empty.
+     */
+    E pop();
+
+    /**
+     * Returns the element at the top of the Stack without removing it.
+     *
+     * @return the element at the top of the Stack.
+     * @throws EmptyStackException if the Stack is empty.
+     */
+    E peek();
+
+    /**
+     * Checks if the Stack is empty.
+     *
+     * @return true if the Stack is empty, false otherwise.
+     */
+    boolean isEmpty();
+
+    /**
+     * Returns the number of elements in the Stack.
+     *
+     * @return the number of elements in the Stack.
+     */
+    int size();
 }
